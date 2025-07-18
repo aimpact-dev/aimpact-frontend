853183138acee92f78d6299f226c11c8370f1633
user <sergioreklamplay@gmail.com>
2025-07-18 10:11:40 +0300

    fix: error message

--- app/routes/leaderboard.tsx
+++ app/routes/leaderboard.tsx
@@ -130,7 +130,7 @@ export default function Leaderboard() {
                 })
               ) : 
                 (isAuthorized ? (
-                    <p className="text-center">Loading...</p>
+                    <p className="text-center">Plesase connect wallet</p>
                   ) : (
                     <p className="text-center">Loading...</p>
                   )
