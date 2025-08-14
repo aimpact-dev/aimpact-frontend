040ddf2a297d1099d17231dba4896a2266e3d40c
Mehanari <102820106+Mehanari@users.noreply.github.com>
2025-08-14 09:16:34 +0300

    Added aimpact preview.

--- app/lib/stores/aimpactPreview.ts
+++ app/lib/stores/aimpactPreview.ts
@@ -1,4 +1,6 @@
 ﻿import type { Sandbox } from '@daytonaio/sdk';
+import type { PortCatcher } from '~/utils/portCatcher';
+import { atom } from 'nanostores';
 
 export interface PreviewInfo {
   port: number;
@@ -8,4 +10,26 @@ export interface PreviewInfo {
 
 export class AimpactPreviewStore {
   private sandbox: Promise<Sandbox>;
+  private portCatcher: PortCatcher;
+
+  previews = atom<PreviewInfo[]>([]);
+
+  constructor(sandbox: Promise<Sandbox>, portCatcher: PortCatcher) {
+    this.sandbox = sandbox;
+    this.portCatcher = portCatcher;
+
+    portCatcher.addCallback(this.onPortChange);
+  }
+
+  private async onPortChange(port: number){
+    this.previews.set([]);
+    const sandbox = await this.sandbox;
+    const url = await sandbox.getPreviewLink(port);
+    const previewInfo: PreviewInfo = {
+      port: port,
+      ready: true,
+      baseUrl: url.url
+    };
+    this.previews.set([previewInfo]);
+  }
 }
