767d31cbed29eb48bb90824a4525ac9055341bd1
Mehanari <102820106+Mehanari@users.noreply.github.com>
2025-09-26 21:50:12 +0300

    Added hooks for working with contracts deploy.

---
+++ app/lib/hooks/tanstack/useContractDeploy.ts
@@ -0,0 +1,38 @@
+﻿import { useMutation } from '@tanstack/react-query';
+import type { AxiosError } from 'axios';
+import { client } from '../../api/backend/api';
+
+export type ContractDeployRequestStatus = 'STARTED' | 'DEPLOYING' | 'COMPLETED' | 'FAILED';
+export type SolanaNetwork = 'devnet' | 'mainnet';
+
+export interface PostDeployRequestPayload {
+  projectId: string;
+  solanaNetwork: 'devnet' | 'mainnet';
+}
+
+export interface GetDeployRequestResponse {
+  projectId: string;
+  programId: string;
+  network: SolanaNetwork;
+  status: ContractDeployRequestStatus;
+  startedAt: Date;
+  message?: string;
+  logs?: Array<string>;
+}
+
+export interface GetDeploymentResponse {
+  programName: string;
+  programId: string;
+  programIds: object;
+  network: SolanaNetwork;
+  deployedAt: Date;
+  upgradeAuthorityPublicKey: string;
+}
+
+export const usePostDeployRequest = () =>
+  useMutation<void, AxiosError, PostDeployRequestPayload>({
+    mutationFn: async (payload) => {
+      const { data } = await client.post('/deploy-contract/deploy-request', payload);
+      return data;
+    }
+  });
