920e95c2258e7d48db39ab2ba5be070ddef450fe
Mehanari <102820106+Mehanari@users.noreply.github.com>
2025-10-18 14:59:11 +0200

    Added errors logging to CloudflareKV.

--- app/lib/persistence/kv/cloudflareKV.ts
+++ app/lib/persistence/kv/cloudflareKV.ts
@@ -8,19 +8,39 @@ export class CloudflareKV implements PersistentKV {
   }
 
   async delete(key: string): Promise<void> {
-    await this.kv.delete(key);
+    try{
+      await this.kv.delete(key);
+    } catch(err){
+      console.log(`Error when deleting key ${key} from Cloudflare KV. Error ${JSON.stringify(err)}`);
+      throw err;
+    }
   }
 
   async exists(key: string): Promise<boolean> {
-    const res = await this.kv.get(key);
-    return !!res;
+    try{
+      const res = await this.kv.get(key);
+      return !!res;
+    } catch(err){
+      console.log(`Error occurred while checking for key ${key} existence in Cloudflare KV. Error ${JSON.stringify(err)}`);
+      throw err;
+    }
   }
 
   async get(key: string): Promise<string | null> {
-    return this.kv.get(key);
+    try{
+      return this.kv.get(key);
+    } catch(err){
+      console.log(`Error occurred while getting key ${key} in Cloudflare KV. Error ${JSON.stringify(err)}`);
+      throw err;
+    }
   }
 
   async set(key: string, value: string): Promise<void> {
-    await this.kv.put(key, value);
+    try{
+      await this.kv.put(key, value);
+    } catch(err){
+      console.log(`Error occurred while setting key ${key} in Cloudflare KV. Error ${JSON.stringify(err)}`);
+      throw err;
+    }
   }
 }
